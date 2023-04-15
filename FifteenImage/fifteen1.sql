f9aefae746cf753f0534726241ceb683c677fd6f
шусис блин <veronika.october@yandex.ru>
2023-04-15 21:00:13 +0300

    FifteenImage-Game

--- FifteenImage/fifteen1.sql
+++ FifteenImage/fifteen1.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 15.2
 -- Dumped by pg_dump version 15.2
 
--- Started on 2023-04-13 15:43:43
+-- Started on 2023-04-15 17:48:58
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -23,14 +23,13 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 219 (class 1259 OID 32841)
+-- TOC entry 219 (class 1259 OID 41031)
 -- Name: game; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.game (
     gameid integer NOT NULL,
     userid integer NOT NULL,
-    username character varying,
     date_game timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     moves integer DEFAULT 0,
     researched_states integer DEFAULT 0
@@ -40,7 +39,7 @@ CREATE TABLE public.game (
 ALTER TABLE public.game OWNER TO postgres;
 
 --
--- TOC entry 218 (class 1259 OID 32840)
+-- TOC entry 218 (class 1259 OID 41030)
 -- Name: game_gameid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -56,7 +55,7 @@ CREATE SEQUENCE public.game_gameid_seq
 ALTER TABLE public.game_gameid_seq OWNER TO postgres;
 
 --
--- TOC entry 3268 (class 0 OID 0)
+-- TOC entry 3265 (class 0 OID 0)
 -- Dependencies: 218
 -- Name: game_gameid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -95,7 +94,7 @@ CREATE SEQUENCE public.information_informationid_seq
 ALTER TABLE public.information_informationid_seq OWNER TO postgres;
 
 --
--- TOC entry 3269 (class 0 OID 0)
+-- TOC entry 3266 (class 0 OID 0)
 -- Dependencies: 214
 -- Name: information_informationid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -104,13 +103,13 @@ ALTER SEQUENCE public.information_informationid_seq OWNED BY public.information.
 
 
 --
--- TOC entry 217 (class 1259 OID 32827)
+-- TOC entry 217 (class 1259 OID 41019)
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.users (
     userid integer NOT NULL,
-    username character varying,
+    username character varying NOT NULL,
     login character varying NOT NULL,
     password_ character varying NOT NULL,
     date_registration timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
@@ -122,7 +121,7 @@ CREATE TABLE public.users (
 ALTER TABLE public.users OWNER TO postgres;
 
 --
--- TOC entry 216 (class 1259 OID 32826)
+-- TOC entry 216 (class 1259 OID 41018)
 -- Name: users_userid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -138,7 +137,7 @@ CREATE SEQUENCE public.users_userid_seq
 ALTER TABLE public.users_userid_seq OWNER TO postgres;
 
 --
--- TOC entry 3270 (class 0 OID 0)
+-- TOC entry 3267 (class 0 OID 0)
 -- Dependencies: 216
 -- Name: users_userid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -147,7 +146,7 @@ ALTER SEQUENCE public.users_userid_seq OWNED BY public.users.userid;
 
 
 --
--- TOC entry 3099 (class 2604 OID 32844)
+-- TOC entry 3099 (class 2604 OID 41034)
 -- Name: game gameid; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -163,7 +162,7 @@ ALTER TABLE ONLY public.information ALTER COLUMN informationid SET DEFAULT nextv
 
 
 --
--- TOC entry 3095 (class 2604 OID 32830)
+-- TOC entry 3095 (class 2604 OID 41022)
 -- Name: users userid; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -171,67 +170,61 @@ ALTER TABLE ONLY public.users ALTER COLUMN userid SET DEFAULT nextval('public.us
 
 
 --
--- TOC entry 3262 (class 0 OID 32841)
+-- TOC entry 3259 (class 0 OID 41031)
 -- Dependencies: 219
 -- Data for Name: game; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.game (gameid, userid, username, date_game, moves, researched_states) VALUES (1, 1, 'Veronika', '2023-04-05 21:28:43.434297', 32, 50000);
-INSERT INTO public.game (gameid, userid, username, date_game, moves, researched_states) VALUES (2, 1, 'Veronika', '2023-04-05 21:28:43.434297', 54, 65000);
-INSERT INTO public.game (gameid, userid, username, date_game, moves, researched_states) VALUES (3, 3, 'Artem', '2023-04-05 21:28:43.434297', 78, 30200);
-INSERT INTO public.game (gameid, userid, username, date_game, moves, researched_states) VALUES (7, 3, 'Artem', '2023-04-13 14:23:00.384891', 47, 286335);
-INSERT INTO public.game (gameid, userid, username, date_game, moves, researched_states) VALUES (4, 1, 'Veronika', '2023-04-05 21:30:03.611999', 56, 200300);
-INSERT INTO public.game (gameid, userid, username, date_game, moves, researched_states) VALUES (5, 1, 'Veronika', '2023-04-09 19:07:07.742079', 74, 345222);
-INSERT INTO public.game (gameid, userid, username, date_game, moves, researched_states) VALUES (6, 3, 'Artem', '2023-04-13 14:22:04.248114', 45, 348888);
+COPY public.game (gameid, userid, date_game, moves, researched_states) FROM stdin;
+1	1	2023-04-15 15:43:15.961152	32	50000
+2	1	2023-04-15 15:43:15.961152	54	65000
+3	3	2023-04-15 15:43:15.961152	78	30200
+4	1	2023-04-15 16:37:05.287329	45	284999
+5	1	2023-04-15 16:58:23.862968	47	234222
+\.
 
 
 --
--- TOC entry 3258 (class 0 OID 32780)
+-- TOC entry 3255 (class 0 OID 32780)
 -- Dependencies: 215
 -- Data for Name: information; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.information (informationid, title, text_info) VALUES (1, 'Правила игры', 'Классическое игровое поле представляет собой матрицу 4х4 клеток, на котором по порядку (слева - направо и сверху - вниз) располагаются цифры от 1 до 15. 
-
-Последняя клетка – пустая. Клетки перемешиваются определенным образом, и задача игрока состоит в том, чтобы восстановить их первоначальное правильное расположение.');
-INSERT INTO public.information (informationid, title, text_info) VALUES (2, 'Про создателя пятнашек', 'Считается, что головоломку «Пятнашки» в 1870-х гг. создал шахматист и составитель шахматных задач Сэм Лойд (1841–1911). 
-
-Она состоит из игрового поля размером N x M, в каждой клетке которого может быть всё что угодно: цифра, буква, изображение и т.д.');
-INSERT INTO public.information (informationid, title, text_info) VALUES (3, 'Пятнашки в информатике', '«Пятнашки» разных размеров с 1960-х годов регулярно используются в исследованиях в области ИИ; 
-
-в частности, на них испытываются и сравниваются алгоритмы поиска в пространстве состояний с разными эвристическими функциямии другими оптимизациями, влияющими на число посещённых в процессе поиска конфигураций головоломки (вершин графа).');
-INSERT INTO public.information (informationid, title, text_info) VALUES (4, 'Новый век', 'Последние годы человек просто не может существовать без компьютера, смартфона и прочих гаджетов. 
-
-Поэтому, разработчики активно этим пользуются и выпускают электронные версии популярных логических и настольных игр.
-
-Пятнашки - не исключение. Сегодня без труда любой пользователь сможет загрузить игру на персональный компьютер или приложение на свой смартфон. 
-Такие игры помогают скоротать время в транспорте по дороге на работу или в длинной очереди.');
+COPY public.information (informationid, title, text_info) FROM stdin;
+1	Правила игры	Классическое игровое поле представляет собой матрицу 4х4 клеток, на котором по порядку (слева - направо и сверху - вниз) располагаются цифры от 1 до 15. \n\nПоследняя клетка – пустая. Клетки перемешиваются определенным образом, и задача игрока состоит в том, чтобы восстановить их первоначальное правильное расположение.
+2	Про создателя пятнашек	Считается, что головоломку «Пятнашки» в 1870-х гг. создал шахматист и составитель шахматных задач Сэм Лойд (1841–1911). \n\nОна состоит из игрового поля размером N x M, в каждой клетке которого может быть всё что угодно: цифра, буква, изображение и т.д.
+3	Пятнашки в информатике	«Пятнашки» разных размеров с 1960-х годов регулярно используются в исследованиях в области ИИ; \n\nв частности, на них испытываются и сравниваются алгоритмы поиска в пространстве состояний с разными эвристическими функциямии другими оптимизациями, влияющими на число посещённых в процессе поиска конфигураций головоломки (вершин графа).
+4	Новый век	Последние годы человек просто не может существовать без компьютера, смартфона и прочих гаджетов. \n\nПоэтому, разработчики активно этим пользуются и выпускают электронные версии популярных логических и настольных игр.\n\nПятнашки - не исключение. Сегодня без труда любой пользователь сможет загрузить игру на персональный компьютер или приложение на свой смартфон. \nТакие игры помогают скоротать время в транспорте по дороге на работу или в длинной очереди.
+\.
 
 
 --
--- TOC entry 3260 (class 0 OID 32827)
+-- TOC entry 3257 (class 0 OID 41019)
 -- Dependencies: 217
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.users (userid, username, login, password_, date_registration, last_login_date, total_games) VALUES (2, 'Alina', 'Alina123', 'Alina123', '2023-04-05 21:28:43.434297', '2023-04-05', 0);
-INSERT INTO public.users (userid, username, login, password_, date_registration, last_login_date, total_games) VALUES (4, 'Rinat', 'Rinat123', 'Rinat123', '2023-04-05 21:28:43.434297', '2023-04-05', 0);
-INSERT INTO public.users (userid, username, login, password_, date_registration, last_login_date, total_games) VALUES (5, 'Lina', 'Lina123', 'Lina123', '2023-04-05 21:28:43.434297', '2023-04-05', 0);
-INSERT INTO public.users (userid, username, login, password_, date_registration, last_login_date, total_games) VALUES (1, 'Veronika', 'Veronika123', 'Veronika123', '2023-04-05 21:28:43.434297', '2023-04-13', 4);
-INSERT INTO public.users (userid, username, login, password_, date_registration, last_login_date, total_games) VALUES (3, 'Artem', 'Artem123', 'Artem123', '2023-04-05 21:28:43.434297', '2023-04-13', 3);
+COPY public.users (userid, username, login, password_, date_registration, last_login_date, total_games) FROM stdin;
+2	Alina	Alina123	Alina123	2023-04-15 15:43:15.961152	2023-04-15	0
+4	Rinat	Rinat123	Rinat123	2023-04-15 15:43:15.961152	2023-04-15	0
+5	Lina	Lina123	Lina123	2023-04-15 15:43:15.961152	2023-04-15	0
+3	Artem	Artem123	Artem123	2023-04-15 15:43:15.961152	2023-04-15	1
+1	Veronika	Veronika123	Veronika123	2023-04-15 15:43:15.961152	2023-04-15	4
+6	Amina	Amina123	Amina123	2023-04-15 17:06:37.561084	2023-04-15	0
+\.
 
 
 --
--- TOC entry 3271 (class 0 OID 0)
+-- TOC entry 3268 (class 0 OID 0)
 -- Dependencies: 218
 -- Name: game_gameid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.game_gameid_seq', 7, true);
+SELECT pg_catalog.setval('public.game_gameid_seq', 5, true);
 
 
 --
--- TOC entry 3272 (class 0 OID 0)
+-- TOC entry 3269 (class 0 OID 0)
 -- Dependencies: 214
 -- Name: information_informationid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
@@ -240,16 +233,16 @@ SELECT pg_catalog.setval('public.information_informationid_seq', 4, true);
 
 
 --
--- TOC entry 3273 (class 0 OID 0)
+-- TOC entry 3270 (class 0 OID 0)
 -- Dependencies: 216
 -- Name: users_userid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.users_userid_seq', 5, true);
+SELECT pg_catalog.setval('public.users_userid_seq', 6, true);
 
 
 --
--- TOC entry 3112 (class 2606 OID 32851)
+-- TOC entry 3110 (class 2606 OID 41039)
 -- Name: game game_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -276,7 +269,7 @@ ALTER TABLE ONLY public.information
 
 
 --
--- TOC entry 3108 (class 2606 OID 32837)
+-- TOC entry 3108 (class 2606 OID 41029)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -285,16 +278,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 3110 (class 2606 OID 32839)
--- Name: users users_username_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_username_key UNIQUE (username);
-
-
---
--- TOC entry 3113 (class 2606 OID 32852)
+-- TOC entry 3111 (class 2606 OID 41040)
 -- Name: game game_userid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -302,16 +286,7 @@ ALTER TABLE ONLY public.game
     ADD CONSTRAINT game_userid_fkey FOREIGN KEY (userid) REFERENCES public.users(userid);
 
 
---
--- TOC entry 3114 (class 2606 OID 32857)
--- Name: game game_username_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.game
-    ADD CONSTRAINT game_username_fkey FOREIGN KEY (username) REFERENCES public.users(username);
-
-
--- Completed on 2023-04-13 15:43:44
+-- Completed on 2023-04-15 17:48:58
 
 --
 -- PostgreSQL database dump complete
